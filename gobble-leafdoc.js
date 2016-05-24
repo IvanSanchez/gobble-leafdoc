fd1b4d8e2bc8fab4cbce8b9cfdc91c4df65da8d3
Iván Sánchez Ortega <ivan@mazemap.no>
2016-05-24 14:30:29 +0200

    Add 🍂doc comments

--- gobble-leafdoc.js
+++ gobble-leafdoc.js
@@ -1,3 +1,35 @@
+/*
+
+🦃namespace leafdoc
+
+Build documentation with [Leafdoc](https://github.com/Leaflet/Leafdoc) docstrings
+
+🦃example
+
+```javascript
+module.exports = gobble( 'src' ).transform( 'leafdoc', {
+output: 'documentation.html',
+templateDir: 'assets/custom-leafdoc-templates',
+leadingCharacter: '@'
+});
+```
+
+🦃option templateDir: String = 'templates/basic'
+Location of the Leafdoc templates to be used, relative to the gobblefile.
+
+🦃option showInheritancesWhenEmpty: Boolean = false
+When `true`, child classes/namespaces will display documentables from ancestors, even if the child class doesn't have any of such documentables.
+
+🦃option leadingCharacter: String = '🍂'
+Overrides the leaf symbol as the leading character for docstrings.
+
+🦃option customDocumentables: Map = {}
+An optional key-value map, each pair will be passed to Leafdoc's `registerDocumentable`.
+
+🦃option verbose: Boolean = false
+When `true`, output some info about files being processed.
+
+*/
 
 var LeafDoc = require('leafdoc');
 var sander = require('sander');
@@ -5,12 +37,28 @@ var path = require('path');
 var sandermatch = require('sandermatch');
 
 function leafdoc ( inputdir, outputdir, options/*, callback */) {
-
 	var doc = new LeafDoc(options),
 	    files;
 
+	// 🦃option output: String = 'leafdoc.html'
+	// The filename where the documentation will be written to.
 	if (!options.output) { options.output = 'leafdoc.html'; }
 	
+	// 🦃option files: Minimatch = undefined
+	// An optional [Minimatch](https://github.com/isaacs/minimatch) expression (or
+	// an array of minimatch expressions) to filter the files by. Works like the
+	// built-in `include` gobble transform, but keeps the order, so this can be
+	// used for leading/trailing content:
+	// ```javascript
+	// module.exports = gobble( 'src' ).transform( 'leafdoc', {
+	// 	output: 'documentation.html',
+	// 	files: [
+	// 	'first-very-important-thing.leafdoc',
+	// 	'**/*.js',
+	// 	'credits.leafdoc'
+	// 	]
+	// });
+	// ```
 	if (options.files) {
 		files = sandermatch.lsrMatch( inputdir, options.files );
 	} else {
@@ -18,7 +66,6 @@ function leafdoc ( inputdir, outputdir, options/*, callback */) {
 	}
 	
 	return files.then( function(filenames) {
-		var logged = false;
 		filenames.forEach(function(filename) {
 			doc.addFile(path.join(inputdir, filename), path.extname(filename) !== '.leafdoc');
 		})
